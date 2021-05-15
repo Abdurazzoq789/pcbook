db0d7b18cf385698ebc670cbd5201d65a7db7e67
Abdurazzoq <arasulmuhammedov@gmail.com>
2021-05-15 18:09:26 +0500

    git ignore fix

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Abdurazzoq789/pcbook
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/google/uuid v1.2.0
+	github.com/stretchr/testify v1.7.0
+	google.golang.org/protobuf v1.26.0
+)
---
+++ sample/generator.go
@@ -0,0 +1,125 @@
+package sample
+
+import (
+	"github.com/Abdurazzoq789/pcbook/pb"
+	_ "github.com/golang/protobuf/ptypes"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func NewKeyBoard() *pb.Keyboard {
+	keyboard := &pb.Keyboard{
+		Layout:  randomKeyboardLayout(),
+		Backlit: randomBool(),
+	}
+	return keyboard
+}
+
+func NewCPU() *pb.CPU {
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+
+	numberCores := randomInt(2, 8)
+	numberThreads := randomInt(numberCores, 12)
+
+	minGhz := randomFloat64(2.0, 3.5)
+	maxGhz := randomFloat64(minGhz, 5.0)
+
+	cpu := &pb.CPU{
+		Brand:         brand,
+		Name:          name,
+		NumberCores:   uint32(numberCores),
+		NumberThreads: uint32(numberThreads),
+		MinGhz:        minGhz,
+		MaxGhz:        maxGhz,
+	}
+
+	return cpu
+}
+
+func NewGPU() *pb.GPU {
+	brand := randomGPUBrand()
+	name := randomGPUName(brand)
+
+	minGhz := randomFloat64(1.0, 1.5)
+	maxGhz := randomFloat64(minGhz, 2.0)
+
+	memory := &pb.Memory{
+		Value: uint64(randomInt(2, 6)),
+		Unit:  pb.Memory_GIGABYTE,
+	}
+
+	gpu := &pb.GPU{
+		Brand:  brand,
+		Name:   name,
+		MinGhz: minGhz,
+		MaxGhz: maxGhz,
+		Memory: memory,
+	}
+	return gpu
+}
+
+func NewRam() *pb.Memory {
+	ram := &pb.Memory{
+		Value: uint64(randomInt(2, 6)),
+		Unit:  pb.Memory_GIGABYTE,
+	}
+	return ram
+}
+
+func NewSSD() *pb.Storage {
+	ssd := &pb.Storage{
+		Driver: pb.Storage_SSD,
+		Memory: &pb.Memory{
+			Value: uint64(randomInt(128, 1024)),
+			Unit:  pb.Memory_GIGABYTE,
+		},
+	}
+	return ssd
+}
+
+func NewHDD() *pb.Storage {
+	ssd := &pb.Storage{
+		Driver: pb.Storage_HDD,
+		Memory: &pb.Memory{
+			Value: uint64(randomInt(1, 6)),
+			Unit:  pb.Memory_TERABYTE,
+		},
+	}
+	return ssd
+}
+
+func NewScreen() *pb.Screen {
+	screen := &pb.Screen{
+		SizeInch:   randomFloat32(13, 17),
+		Resolution: randomScreenResolution(),
+		Panel:      randomScreenPannel(),
+		Multitouch: randomBool(),
+	}
+
+	return screen
+}
+
+func NewLaptop() *pb.Laptop {
+	brand := randomLaptopBrand()
+	name := randomLaptopName(brand)
+
+	laptop := &pb.Laptop{
+		Id:       randomId(),
+		Brand:    brand,
+		Name:     name,
+		Cpu:      NewCPU(),
+		Ram:      NewRam(),
+		Gpus:     []*pb.GPU{NewGPU()},
+		Storages: []*pb.Storage{NewSSD(), NewHDD()},
+		Screen:   NewScreen(),
+		Keyboard: NewKeyBoard(),
+		Weight: &pb.Laptop_WeightKg{
+			WeightKg: randomFloat64(1.0, 3.0),
+		},
+		PriceUsd:    randomFloat64(1500, 3000),
+		ReleaseYear: uint32(randomInt(2015, 2021)),
+		UpdatedAt:   timestamppb.Now(),
+	}
+
+	return laptop
+}
