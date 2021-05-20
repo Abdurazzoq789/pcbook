45cf24ec5a625117efca4f015c9bfd34a69ffd52
Abdurazzoq <arasulmuhammedov@gmail.com>
2021-05-20 09:49:00 +0500

    jwt auth

---
+++ client/auth_client.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"context"
+	"github.com/Abdurazzoq789/pcbook/pb"
+	"google.golang.org/grpc"
+	"time"
+)
+
+type AuthClient struct {
+	service pb.AuthServiceClient
+	username string
+	password string
+}
+
+func NewAuthClient(cc *grpc.ClientConn, username string, password string) *AuthClient {
+	service := pb.NewAuthServiceClient(cc)
+	return &AuthClient{service, username, password}
+}
+
+func (client *AuthClient) Login() (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	req := &pb.LoginRequest{
+		Username: client.username,
+		Password: client.password,
+	}
+
+	res, err := client.service.Login(ctx, req)
+	if err != nil {
+		return "", err
+	}
+
+	return res.GetAccessToken(), nil
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/Abdurazzoq789/pcbook
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.2.0
 	github.com/jinzhu/copier v0.3.0
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
 	google.golang.org/grpc v1.37.1
 	google.golang.org/protobuf v1.26.0
 )
